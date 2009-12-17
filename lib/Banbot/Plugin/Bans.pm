5a152e83858cbb5e724fc9c4f44998d900d3f7ad
Karl Thornton <eliza.fritzl@gmail.com>
2009-12-17 19:18:34 +0000

    test

--- lib/Banbot/Plugin/Bans.pm
+++ lib/Banbot/Plugin/Bans.pm
@@ -6,9 +6,9 @@ use Data::Dumper;
 use namespace::autoclean;
 
 sub S_chan_mode {
-    my ($self, $irc, @args) = @_;
-    warn Data::Dumper->Dumper(\@args);
-    warn Data::Dumper->Dumper($irc->channel_ban_list(${$args[1]}))
+    my ( $self, $irc, @args ) = @_;
+    warn Data::Dumper->Dumper( \@args );
+    warn Data::Dumper->Dumper( $irc->channel_ban_list(${$args[1]}) )
 }
 
 1;

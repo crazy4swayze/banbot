c5cd0f8c858285e401f664684af707a2414c9abd
Karl Thornton <eliza.fritzl@gmail.com>
2009-12-17 20:40:55 +0000

    updated Banbot::Plugin::Bans and updated log files

--- lib/Banbot/Plugin/Bans.pm
+++ lib/Banbot/Plugin/Bans.pm
@@ -5,10 +5,21 @@ use Data::Dumper;
 
 use namespace::autoclean;
 
+sub S_bot_addressed {
+    my ( $self, $irc, $nickstring, $channels, $message ) = @_;
+    if ( $$message eq 'showbans' ) {
+        warn 'showbans';
+        warn Data::Dumper->Dumper( $self->irc->channel_ban_list($$channels->[0]) )
+    }
+    return PCI_EAT_PLUGIN;
+}
+
 sub S_chan_mode {
     my ( $self, $irc, @args ) = @_;
+    warn 'mode change';
     warn Data::Dumper->Dumper( \@args );
-    warn Data::Dumper->Dumper( $irc->channel_ban_list(${$args[1]}) )
+    warn Data::Dumper->Dumper( $self->irc->channel_ban_list(${$args[1]}) )
+    return PCI_EAT_PLUGIN;
 }
 
 1;
